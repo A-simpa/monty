97da67a007e9add4fcb07ff4ae0afcd75dde5696
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-22 09:37:58 +0000

    added files for pull and push

---
+++ pall.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * pall - prints the elements in a double linked list
+ * @h: the head of the double linked list
+ *
+ * Return: the length of the double linked list
+ */
+
+size_t pall(const stack_t *h)
+{
+	size_t len = 0;
+
+	line_number = 0;
+	if (h)
+	{
+		do {
+			printf("%d\n", h->n);
+			len++;
+		} while ((h = h->next) != NULL);
+	}
+	return (len);
+}
---
+++ push.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * push - add an integer to the begining
+ * @n: the integer to add at the front
+ * @head: pointer to the head of the dlist
+ *
+ * Return: the address of the new int
+ */
+
+stack_t *push(stack_t **head, unsigned int n)
+{
+	stack_t *new;
+
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->prev = NULL;
+	new->n = n;
+	if (*head)
+	{
+		(*head)->prev = new;
+	}
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
