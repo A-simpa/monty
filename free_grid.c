120bbb4d3863d72a0fc3277fa142d09b0f75199e
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-22 14:32:03 +0000

    added more files

---
+++ free_grid.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+*free_grid - frees a 2d array
+*@grid: 2d array
+*@height: height
+*Return: void
+*/
+
+void free_grid(char **grid)
+{
+int k = 0;
+
+int height = countstr(grid);
+
+while (k < height)
+{
+	free(grid[k]);
+	k++;
+}
+free(grid);
+}
+
---
+++ free_stack.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ *free_dlistint - frees a dlist
+ *@head: the first node
+ *
+ *Return: void
+ */
+
+void free_stack(stack_t *head)
+{
+	stack_t *h;
+	size_t len, count, times;
+
+	if (head)
+	{
+		count = 0, len = dlistint_len(head) - 1;
+		while (count < len)
+		{
+			h = head;
+			times = len - count;
+			do {
+				h = h->next;
+			} while ((--times) > 0);
+			free(h);
+			count++;
+		}
+	}
+	free(head);
+}
+
