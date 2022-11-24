25136be48c120907b95b80a21b31df2f31721706
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-24 10:45:08 +0000

    added implentation of nop to error

--- _error.c
+++ _error.c
@@ -13,10 +13,9 @@
 
 void _error(char **str, int line_count, char *line, FILE *fp, stack_t *s)
 {
-	int all_y, ush_y, int_y, op_y;
+	int ush_y, nop_y;
 
-	ush_y = streq(str[0], "push"), all_y = streq(str[0], "pall");
-	int_y = streq(str[0], "pint"), op_y = streq(str[0], "pop");
+	ush_y = streq(str[0], "push"), nop_y = streq(str[0], "nop");
 	if (ush_y == 0 && countstr(str) < 2)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_count);
@@ -25,7 +24,7 @@ void _error(char **str, int line_count, char *line, FILE *fp, stack_t *s)
 			free_stack(s);
 		exit(EXIT_FAILURE);
 	}
-	else if (all_y != 0 && ush_y != 0 && int_y != 0 && op_y != 0)
+	else if (ush_y != 0 && nop_y != 0)
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_count, str[0]);
 		free(line), free_grid(str), fclose(fp);
