d29044b106478f76a888669f1039e2e711597975
A-simpa <mohammedabdulmujeeb16@gmail.com>
2022-11-24 14:40:13 +0000

    added # and mod

---
+++ mod.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * mods - mod the top by the next node
+ * @s: the pointer to the first head
+ * @lc: the line count
+ * @cmd: the command line
+ * @line: the current
+ * @fp: the file pointer
+ *
+ * Return: void
+ */
+
+void mods(stack_t **s, int lc, char **cmd, char *line, FILE *fp)
+{
+	int sum = 0;
+	stack_t *h = *s;
+
+	if (stack_len(*s) >= 2)
+	{
+		if ((*s)->n == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", lc);
+			free(line), free_grid(cmd), free_stack(*s), fclose(fp);
+			exit(EXIT_FAILURE);
+		}
+		h = h->next;
+		sum = (h->n) % ((*s)->n);
+		pop(s, lc, cmd, line,  fp);
+		pop(s, lc, cmd, line, fp);
+		add_stack(s, sum);
+		return;
+	}
+	fprintf(stderr, "L%d: can't mod, stack too short\n", lc);
+	free(line), free_grid(cmd), free_stack(*s), fclose(fp);
+	exit(EXIT_FAILURE);
+}
